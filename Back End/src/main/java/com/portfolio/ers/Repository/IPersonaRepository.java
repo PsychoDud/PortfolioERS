bc029b3997e02a109ea95a46ba64bcc671187582
PsychoDud <emanuelstaffa@gmail.com>
2022-08-22 20:30:52 -0300

    version final

---
+++ Back End/src/main/java/com/portfolio/ers/Repository/IPersonaRepository.java
@@ -0,0 +1,10 @@
+package com.portfolio.ers.Repository;
+
+import com.portfolio.ers.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IPersonaRepository extends JpaRepository<Persona,Long> {
+    
+}
